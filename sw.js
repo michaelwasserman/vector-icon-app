c921699fa91573ffd6dd831f7ac26612d3d501e6
Mike Wasserman <msw@chromium.org>
2019-10-22 16:04:57 -0700

    Fix filesToCache paths

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'chromium-vector-icon-app';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
