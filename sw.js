517219edbf545810387423c5633601c5ee4fed4f
Mike Wasserman <msw@chromium.org>
2020-01-16 19:01:01 -0800

    Fix colors, cleanup

--- sw.js
+++ sw.js
@@ -10,21 +10,17 @@ var filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   console.log("INFO: service worker install");
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(function(cache) {
+    return cache.addAll(filesToCache);
+  }));
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   // console.log("INFO: service worker fetch");
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(function(response) {
+    return response || fetch(e.request);
+  }));
 });
 
 self.addEventListener('message', function(event){
@@ -38,7 +34,7 @@ async function openWindow(url, bounds) {
   // Use runtime feature detection for the Window Placement API.
   if (clients.openWindow.length == 1) {
     console.log("Please enable to Window Placement API: " +
-                "https://github.com/spark008/window-placement");
+                "https://github.com/webscreens/window-placement");
     var options = "left=" + bounds.left + ",top=" + bounds.top +
                   ",width=" + bounds.width + ",height=" + bounds.height;
     console.log("MSW calling window.open with options: " + options);
